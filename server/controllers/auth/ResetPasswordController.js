d9669bedee52a15b3b8a7dc0de734c7e5978656c
Vikas Sarkar <VIKASSAR@amdocs.com>
2017-06-16 12:58:40 +0530

    Reset pwd code  refactor

--- server/controllers/auth/ResetPasswordController.js
+++ server/controllers/auth/ResetPasswordController.js
@@ -5,6 +5,11 @@
 
 'use strict';
 
+/**
+ * Login api url handler
+ * @connectionErrors - generic db connection errors handler
+ * @resetUtils - reset password utility functions for all data manupulation and curd operations
+ */
 var connectionErrors = require('../../utilities/ConnectionErrors'),
     resetUtils = require('./utils/ResetPasswordUtils');
 
@@ -18,42 +23,38 @@ module.exports = function(app, route, dbConnection){
 	});
 
     /**
-	 * Logging in with provided params
-	 * @UserEmail: provided usremail or //not null //from auth
-	 * @UserName: provided username and //not null //from auth
-	 * @UserPassword: provided password //not null //from auth
+	 * map temporary password to reset password
+	 * @req - request params for api
+	 * @resp - response tobe send
+	 * @req param {* ||} UserEmail
+	 * @req param {* ||} UserName
+	 * @req param {*} TempPassword
+	 * @req param {*} UserPassword
 	 */
 	app.post('/resetpwd/auth', function (req, resp) {
         dbConnection.getConnection(function (err, connection) {
-			console.log('::::::::::::::::::in mysql pool connection::::::::::::::::::');
+			console.log('::::in mysql pool connection::::::');
 			if (err) {
 				connectionErrors.connectionError(err, connection);
 			}			
 			var reqData = req.body;
-			dbConnection.query('SELECT * from auth WHERE UserEmail=? or UserName=?',
-			[reqData.UserEmail, reqData.UserName], 
-				function(err, authdetails, fields){					
-					if (!err) {
-						console.log('::::::::::::::::::Got data::::::::::::::::::');
-						resetUtils.changePassword(dbConnection, reqData, authdetails, resp, connection);
-					}else{                
-						connectionErrors.queryError(err, connection);
-					}
-				}
-			);
+			var resetPassword = new resetUtils.resetTemporaryPassword(dbConnection, reqData, resp, connection);
+			resetPassword.execute();
 		});
 	});
 
 	/**
      * Backup API if API doesn't exist at all
+	 * @req - request params for api
+	 * @resp - response tobe send
      */
 	app.get('/resetpwd/:id', function(req, res) {
 		res.send('respond with a home data with id - '+ req.params.id);
 	});
 
-	/*
-     **Return middleware.
-     */
+	/**
+	 * Return middleware.
+	 */
     return function(req, res, next) {
         next();
     };
--- server/controllers/auth/utils/ResetPasswordUtils.js
+++ server/controllers/auth/utils/ResetPasswordUtils.js
@@ -14,185 +14,229 @@ var authenticateModels = require('../../../models/auth/AuthenticateModels'),
 
 
 /**
- * Get Old passwords list and check updation of it
+ * Class for authenticating user, code needs to be connected before calling it
+ * @param {*} dbConnection 
+ * @param {*} reqData 
+ * @param {*} resp 
+ * @param {*} connection 
  */
-var _getOldPasswordsList = function(dbConnection, reqData, authdetails, resp, connection){
+var resetTemporaryPassword = function(dbConnection, reqData, resp, connection){
+    var currentClass = this;
+    var privateMethod = {};
+    this.dbConnection = dbConnection;
+    this.reqData = reqData;        
+    this.resp = resp;
+    this.connection = connection;
+    this.oldpasswords = [];
+    this.authdetails = [];
+    this.userdetails = [];
 
-    var oldpasswords = authdetails[0].OldPasswords ? authdetails[0].OldPasswords.split('___') : [];
-    console.log("::::::::::::compared last passd with current::::::::::::");
-
-    if(oldpasswords.length > 0 && oldpasswords[oldpasswords.length - 1] == authdetails[0].UserPassword){
-        console.log('::::::::::old passwords up to date::::::::::')
-        //match temp password
-        _mapTempPassword(dbConnection, reqData, authdetails, oldpasswords, resp, connection);
-    }else{
-        if(oldpasswords.length == 3){
-            oldpasswords.shift();
-            oldpasswords.push(authdetails[0].UserPassword)
-        }else{
-            oldpasswords.push(authdetails[0].UserPassword)
-        }
-        console.log(':::::::::::::update Old Password:::::::::::::');
-        _updateOldPasswordsList(dbConnection, oldpasswords, reqData, authdetails, resp, connection);
-    }
-
-}
-
-/**
- * updating old password list in DB
- */
-var _updateOldPasswordsList = function(dbConnection, oldpasswords, reqData, authdetails, resp, connection){
-    //update old paswwords list
-    dbConnection.query('UPDATE auth SET Oldpasswords = ? WHERE CustomerId = ?',
-        [oldpasswords.join('___'), authdetails[0].CustomerId],
-        function (err, rows, fields) {
-            console.log(':::::::::::::in saving Old Password:::::::::::::');
-            if (!err) {
-                console.log(':::::::::::::Old Password saved:::::::::::::'+oldpasswords.join('___'));
-                //match temp password
-                _mapTempPassword(dbConnection, reqData, authdetails, oldpasswords, resp, connection);
-            } else {
-                console.log(':::::::::::::Cannot save Old Password:::::::::::::');
-                //connection released                
-                connectionErrors.queryError(err, connection);
-            }
-        }
-    );
-}
-
-/**
- * Compare hash password
- */
-var _changePassword = function (dbConnection, reqData, authdetails, resp, connection) {
-    _getOldPasswordsList(dbConnection, reqData, authdetails, resp, connection);
-}
-
-/**
- * check if user had provided proper TempPassword
- */
-var _mapTempPassword = function(dbConnection, reqData, authdetails, oldpasswords, resp, connection){
-    console.log(authdetails[0].TempPassword)
-    console.log(reqData.TempPassword)
-    if (authdetails[0].TempPassword && reqData.TempPassword == authdetails[0].TempPassword) {
-        console.log('::::::::::::::temp matched::::::::::::::::');  
-        _mapWithPreviousThreePassword(dbConnection, reqData, authdetails, oldpasswords, resp, connection);
-    }else{
-        console.log('::::::::::::::temp not matched::::::::::::::::');   
-        //connection released       
-        connection.release();
-        resp.send('temp not matched');
-    }
-}
-
-/**
- * map New password with three old passwords
- */
-var _mapWithPreviousThreePassword = function (dbConnection, reqData, authdetails, oldpasswords, resp, connection) {
-    if(Object.keys(oldpasswords).length > 0){
-
-        //Mapping first old password
-        bcrypt.compare(reqData.UserPassword, oldpasswords[0]).then(function (res) {
-            console.log(":::::::::::::response1:::::::::::::"+oldpasswords[0]+"::::"+res)
-            if(!res){
-                if(Object.keys(oldpasswords).length > 1){
-
-                    //Mapping second old password
-                    bcrypt.compare(reqData.UserPassword, oldpasswords[1]).then(function (res1) {
-                        console.log("::::::::::::::::response2:::::::::::"+oldpasswords[1]+"::::"+res)
-                        if(!res1){
-                            if(Object.keys(oldpasswords).length > 2){
-
-                                //Mapping third old password
-                                bcrypt.compare(reqData.UserPassword, oldpasswords[2]).then(function (res2) {
-                                    
-                                    console.log("::::::::::::::::response3::::::::::::::::"+oldpasswords[2]+"::::"+res)
-                                    if(!res2){
-                                        _saveNewPassword(dbConnection, reqData, authdetails, resp, connection);
-                                    }else{
-                                        //connection released
-                                        customErrors.NewPasswordMatchedOld(resp, connection);
-                                    }
-                                }, function (err) {
-                                    console.log('::::::::::::error in bycryption::::::::::::')
-                                    //connection released
-                                    customErrors.cannotSaveUser(resp, connection);
-                                });
-                            }else{
-                                _saveNewPassword(dbConnection, reqData, authdetails, resp, connection);                                
-                            }
-                        }else{
-                            //connection released
-                            customErrors.NewPasswordMatchedOld(resp, connection);
-                        }
-                    }, function (err) {
-                        console.log('::::::::::::error in bycryption::::::::::::')
-                        //connection released
-                        customErrors.cannotSaveUser(resp, connection);
-                    });
-                }else{                    
-                    _saveNewPassword(dbConnection, reqData, authdetails, resp, connection);
-                }
-            }else{
-                //connection released
-                customErrors.NewPasswordMatchedOld(resp, connection);
-            }
-        }, function (err) {
-            console.log('::::::::::::error in bycryption::::::::::::')
-            //connection released
-            customErrors.cannotSaveUser(resp, connection);
-        });
-    }else{
-        _saveNewPassword(dbConnection, reqData, authdetails, resp, connection);
-    }
+    /**
+     * @privillidged method
+     * initilize authenticateUser methods
+     */
+    this.execute = function(){       
+        privateMethod._getAuthUser();
+    };
     
-}
+    /**
+     * @private method
+     * check if user had provided proper TempPassword
+     */
+    privateMethod._checkTempPassword = function(){
+        if (currentClass.authdetails[0].TempPassword && currentClass.reqData.TempPassword == currentClass.authdetails[0].TempPassword) {
+            console.log(':::::temp matched::::::');  
+            privateMethod._mapWithPreviousThreePassword();
+        }else{
+            console.log('::::temp not matched:::::');   
+            //connection released       
+            currentClass.connection.release();
+            currentClass.resp.send('temp not matched');
+        }
+    };
 
-/**
- * Save New password in DB
- */
-var _saveNewPassword = function (dbConnection, reqData, authdetails, resp, connection) {
-    bcrypt.hash(reqData.UserPassword, envConfig.hashSaltRounds).then(function (hashPassword) {
-        console.log('::::::::::::::::::Got Hash password::::::::::::::::::');
-        dbConnection.query('UPDATE auth SET UserPassword = ? WHERE CustomerId = ?',
-            [hashPassword, authdetails[0].CustomerId],
+    /**
+     * @private method
+     * @DBQuery
+     * query to get authdetails
+     */
+    privateMethod._getAuthUser = function(){
+        dbConnection.query('SELECT * from auth WHERE UserEmail=? or UserName=?',
+			[reqData.UserEmail, reqData.UserName], 
+				function(err, authdetails, fields){					
+					if (!err) {
+						console.log(':::::Got auth data::::::');
+                        currentClass.authdetails = authdetails;
+						privateMethod._setOldPasswordsList();
+					}else{                
+                        //connection released    
+						connectionErrors.queryError(err, connection);
+					}
+				}
+		);
+    };
+
+    /**
+     * @private method
+     * Get Old passwords list and check updation of it
+     */
+    privateMethod._setOldPasswordsList = function(){
+        var oldpasswords = currentClass.authdetails[0].OldPasswords ? currentClass.authdetails[0].OldPasswords.split('___') : [];
+        console.log(":::::compare last passd with current:::::::::");
+
+        if(oldpasswords.length > 0 && oldpasswords[oldpasswords.length - 1] == currentClass.authdetails[0].UserPassword){
+            console.log(':::::old passwords up to date:::::');
+            currentClass.oldpasswords = oldpasswords;
+            //match temp password
+            privateMethod._checkTempPassword();
+        }else{
+            if(oldpasswords.length == 3){
+                oldpasswords.shift();
+                oldpasswords.push(currentClass.authdetails[0].UserPassword)
+            }else{
+                oldpasswords.push(currentClass.authdetails[0].UserPassword)
+            }
+            currentClass.oldpasswords = oldpasswords;
+            privateMethod._updateOldPasswordsList();
+        }
+    };
+
+   /**
+     * @private method
+     * @DBQuery
+     * updating old password list in DB
+     */
+    privateMethod._updateOldPasswordsList = function(){
+        dbConnection.query('UPDATE auth SET Oldpasswords = ? WHERE CustomerId = ?',
+            [currentClass.oldpasswords.join('___'), currentClass.authdetails[0].CustomerId],
             function (err, rows, fields) {
-                console.log(':::::::::::::in saving New Password:::::::::::::');
                 if (!err) {
-                    console.log(':::::::::::::New Password saved:::::::::::::');
-                    _removeTempPassword(dbConnection, reqData, authdetails, resp, connection)
+                    console.log('::::::Old Password saved:::::');
+                    //match temp password
+                    privateMethod._checkTempPassword();
                 } else {
-                    console.log(':::::::::::::Cannot save New Password:::::::::::::');
+                    //connection released                
+                    connectionErrors.queryError(err, currentClass.connection);
+                }
+            }
+        );
+    };
+    
+   /**
+     * @private method
+     * map New password with three old passwords
+     */
+    privateMethod._mapWithPreviousThreePassword = function () {
+        if(Object.keys(currentClass.oldpasswords).length > 0){
+
+            //Mapping first old password
+            bcrypt.compare(currentClass.reqData.UserPassword, currentClass.oldpasswords[0]).then(function (res) {
+                console.log("::::::Old pwd check 1:::::::");
+                if(!res){
+                    if(Object.keys(currentClass.oldpasswords).length > 1){
+
+                        //Mapping second old password
+                        bcrypt.compare(currentClass.reqData.UserPassword, currentClass.oldpasswords[1]).then(function (res1) {
+                           console.log("::::::Old pwd check 2:::::::");
+                            if(!res1){
+                                if(Object.keys(currentClass.oldpasswords).length > 2){
+
+                                    //Mapping third old password
+                                    bcrypt.compare(currentClass.reqData.UserPassword, currentClass.oldpasswords[2]).then(function (res2) {
+                                        
+                                        console.log("::::::Old pwd check 3:::::::");
+                                        if(!res2){
+                                            privateMethod._saveNewPassword();
+                                        }else{
+                                            //connection released
+                                            customErrors.NewPasswordMatchedOld(currentClass.resp, currentClass.connection);
+                                        }
+                                    }, function (err) {
+                                        console.log(':::::error in bycryption::::::')
+                                        //connection released
+                                        customErrors.cannotSaveUser(currentClass.resp, currentClass.connection);
+                                    });
+                                }else{
+                                    privateMethod._saveNewPassword();                                
+                                }
+                            }else{
+                                //connection released
+                                customErrors.NewPasswordMatchedOld(currentClass.resp, currentClass.connection);
+                            }
+                        }, function (err) {
+                            console.log(':::::error in bycryption::::::')
+                            //connection released
+                            customErrors.cannotSaveUser(resp, connection);
+                        });
+                    }else{                    
+                        privateMethod._saveNewPassword();
+                    }
+                }else{
+                    //connection released
+                    customErrors.NewPasswordMatchedOld(currentClass.resp, currentClass.connection);
+                }
+            }, function (err) {
+                console.log(':::::error in bycryption:::::::')
+                //connection released
+                customErrors.cannotSaveUser(currentClass.resp, currentClass.connection);
+            });
+        }else{
+            privateMethod._saveNewPassword();
+        }
+    };
+
+    
+   /**
+     * @private method
+     * @DBQuery
+     * Save New password in DB
+     */
+    privateMethod._saveNewPassword = function () {
+        bcrypt.hash(currentClass.reqData.UserPassword, envConfig.hashSaltRounds).then(function (hashPassword) {
+            dbConnection.query('UPDATE auth SET UserPassword = ? WHERE CustomerId = ?',
+                [hashPassword, currentClass.authdetails[0].CustomerId],
+                function (err, rows, fields) {
+                    if (!err) {
+                        console.log('::::New Password saved:::::');
+                        privateMethod._removeTempPassword()
+                    } else {
+                        //connection released                
+                        connectionErrors.queryError(err, currentClass.connection);
+                    }
+                }
+            );
+        }, function (err) {
+            //connection released
+            customErrors.cannotSaveUser(currentClass.resp, currentClass.connection);
+        });
+    };
+
+   /**
+     * @private method
+     * @DBQuery
+     * Remove temp password if New password saved
+     */
+    privateMethod._removeTempPassword = function(){
+        dbConnection.query('UPDATE auth SET TempPassword = ? WHERE CustomerId = ?',
+            [null, currentClass.authdetails[0].CustomerId],
+            function (err, rows, fields) {
+                if (!err) {
+                    console.log(':::::Temp Password Removed::::::');
+                    resp.send('passwordSaved')
+
+                } else {
                     //connection released                
                     connectionErrors.queryError(err, connection);
                 }
             }
         );
-    }, function (err) {
-        console.log('::::::::::::Got Error in Hash password:::::::::::::::');
-        //connection released
-        customErrors.cannotSaveUser(resp, connection);
-    });
+    };
+
 }
 
 /**
- * Remove temp password if New password saved
+ * Export all exposable Class
  */
-var _removeTempPassword = function(dbConnection, reqData, authdetails, resp, connection){
-    dbConnection.query('UPDATE auth SET TempPassword = ? WHERE CustomerId = ?',
-        [null, authdetails[0].CustomerId],
-        function (err, rows, fields) {
-            if (!err) {
-                console.log(':::::::::::::Temp Password Removed:::::::::::::');
-                resp.send('passwordSaved')
-
-            } else {
-                //connection released                
-                connectionErrors.queryError(err, connection);
-            }
-        }
-    );
-}
-
 module.exports = {
-    'changePassword': _changePassword,
+    'resetTemporaryPassword': resetTemporaryPassword,
 }
